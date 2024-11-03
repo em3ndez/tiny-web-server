c32222138768c420ada370b09c16e347d5794496
Paul Hammant <paul@hammant.org>
2024-11-03 08:48:00 +0000

    feat: Add main method to run tests with Cuppa framework

--- NewTests.java
+++ NewTests.java
@@ -1,12 +1,16 @@
 import com.paulhammant.tnywb.TinyWeb;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import org.forgerock.cuppa.Runner;
 import org.forgerock.cuppa.Test;
+import org.forgerock.cuppa.reporters.DefaultReporter;
+
 import static org.forgerock.cuppa.Cuppa.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Test
 public class NewTests {
@@ -41,5 +45,10 @@ public class NewTests {
         return new OkHttpClient().newCall(new Request.Builder()
                 .url("http://localhost:8080" + url)
                 .get().build()).execute();
+    }
+
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.run(runner.defineTests(Collections.singletonList(NewTests.class)), new DefaultReporter());
     }
 }
