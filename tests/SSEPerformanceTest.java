707020beae7bf1735398187569b00b144bc28f4a
Paul Hammant <paul@hammant.org>
2024-12-29 10:12:38 +0000

    feat: Enhance SSE performance test with detailed connection statistics

--- tests/SSEPerformanceTest.java
+++ tests/SSEPerformanceTest.java
@@ -1,102 +1,150 @@
 package tests;
 
 import com.paulhammant.tiny.Tiny;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ConnectException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.Map;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static tests.Suite.httpGet;
+import static tests.WebSocketBroadcastDemo.sleepMillis;
 
 public class SSEPerformanceTest {
 
+    public static class SseStat {
+
+        public boolean connectExcpetion;
+        public boolean serverIOE;
+        public boolean connected2;
+        public boolean connected;
+        public int messagesReceived;
+        public int clientIOE;
+    }
+
     public static void main(String[] args) {
 
+        Map<Integer, SseStat> stats = new ConcurrentHashMap<>();
 
         long startTime = System.currentTimeMillis();
-        AtomicInteger connectExceptions = new AtomicInteger(0);
-        AtomicInteger otherIOExceptions = new AtomicInteger(0);
 
-        Tiny.WebServer server = new Tiny.WebServer(Tiny.Config.create().withWebPort(8080)) {{
+        Tiny.WebServer server = new Tiny.WebServer(Tiny.Config.create().withHostAndWebPort("localhost", 8080)) {
+            @Override
+            protected HttpServer makeHttpServer() throws IOException {
+                return super.makeHttpServer();
+            }
+
+            @Override
+            protected void newHttpExchange(HttpExchange exchange) {
+            }
+
+            @Override
+            protected void exceptionDuringHandling(Throwable e, HttpExchange exchange) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+            @Override
+            protected void serverException(Tiny.ServerException e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+
+            {
             endPoint(Tiny.HttpMethods.GET, "/sse", (req, res, ctx) -> {
                 res.setHeader("Content-Type", "text/event-stream");
                 res.setHeader("Cache-Control", "no-cache");
                 res.setHeader("Connection", "keep-alive");
+                String client = req.getHeaders().get("sse_perf_client").getFirst();
                 try {
                     res.sendResponseHeaders(200, 0);
                     OutputStream outputStream = res.getResponseBody();
-                    for (int i = 0; i < 10; i++) {
-                        outputStream.write(("data: Message " + i + "\n\n").getBytes());
+                    int i = 0;
+                    while (true) {
+                        outputStream.write(("data: Message " + i++ + "\n\n").getBytes());
                         outputStream.flush();
-//                        Thread.sleep(new java.util.Random().nextInt(2001));
+                        try {
+                            Thread.sleep(new java.util.Random().nextInt(2001)); // average 1 second
+                        } catch (InterruptedException e) {
+                        }
                     }
-//                } catch (InterruptedException e) {
                 } catch (ConnectException e) {
-                    connectExceptions.incrementAndGet();
+                    SseStat stat = stats.get(Integer.parseInt(client));
+                    if (stat == null) {
+                        stat = new SseStat();
+                        stats.put(Integer.parseInt(client), stat);
+                    }
+                    stat.connectExcpetion = true;
                 } catch (IOException e) {
-                    otherIOExceptions.incrementAndGet();
+                    SseStat stat = stats.get(Integer.parseInt(client));
+                    if (stat == null) {
+                        stat = new SseStat();
+                        stats.put(Integer.parseInt(client), stat);
+                    }
+                    stat.serverIOE = true;
                 }
 
             });
         }};
         server.start();
 
-        AtomicInteger successfulConnections = new AtomicInteger(0);
-        AtomicInteger failedConnections = new AtomicInteger(0);
-        AtomicInteger messagesReceived = new AtomicInteger(0);
-
-        int clients = 10000;
+        int clients = 41;
         for (int i = 0; i < clients; i++) {
+            int finalI = i;
             Thread.ofVirtual().start(() -> {
+                SseStat stat = stats.get(finalI);
+                if (stat == null) {
+                    stat = new SseStat();
+                    stats.put(finalI, stat);
+                }
+                stat.connected = true;
+
                 try (okhttp3.Response response = httpGet("/sse")) {
-                    successfulConnections.incrementAndGet();
+                    stat.connected2 = true;
+
                     assertThat(response.code(), equalTo(200));
                     try (BufferedReader reader = new BufferedReader(new InputStreamReader(response.body().byteStream()))) {
-                        for (int j = 0; j < 10; j++) {
-                            assertThat(reader.readLine(), equalTo("data: Message " + j));
-                            messagesReceived.incrementAndGet();
+                        int j = 0;
+                        while (true) {
+                            assertThat(reader.readLine(), equalTo("data: Message " + j++));
+                            stat.messagesReceived = stat.messagesReceived +1;
                             assertThat(reader.readLine(), equalTo(""));
                         }
                     }
                 } catch (IOException e) {
-                    failedConnections.incrementAndGet();
+                    stat.clientIOE = stat.clientIOE +1;
                     e.printStackTrace();
                 }
             });
         }
 
+        sleepMillis(30000);
 
-        ScheduledExecutorService statsScheduler = Executors.newScheduledThreadPool(1);
-        statsScheduler.scheduleAtFixedRate(() -> {
-            long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
-            System.out.println(elapsedTime + " secs: Server connect exceptions: " + connectExceptions.get());
-            System.out.println("Server other IO exceptions: " + otherIOExceptions.get());
+//        System.out.println("Took " + (System.currentTimeMillis() - startTime) + "ms");
 
-            System.out.println("Successful client connections: " + successfulConnections.get());
-            System.out.println("Failed client connections: " + failedConnections.get());
-            System.out.println("Client Messages received: " + messagesReceived.get());
-        }, 0, 10, TimeUnit.SECONDS);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            long elapsed = (System.currentTimeMillis() - startTime) / 1000;
 
-        // Ensure the scheduler is properly shut down
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            statsScheduler.shutdown();
-            server.stop();
-            try {
-                if (!statsScheduler.awaitTermination(5, TimeUnit.SECONDS)) {
-                    statsScheduler.shutdownNow();
-                }
-            } catch (InterruptedException e) {
-                statsScheduler.shutdownNow();
-            }
-        }));
+            stream of stats here -
+                    1. how many rows
+                    2. how many connectExcpetion == true
+                    3. how many connected == true
+                    4. how many connected2 == true
+                    5. total of messagesReceived
+                    5. total of clientIOE
+
+
+            float rate = ((float) successfulConnections.get() * elapsed / messagesReceived.get()) * 100;
+            System.out.printf("At %d secs, THOSE NUMBERS ABOVE", elapsed, ,,,,);
+        }, 10, 10, TimeUnit.SECONDS);
 
     }
 }
