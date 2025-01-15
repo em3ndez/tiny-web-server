210dcf8237a97fb1a14d4091eb194a7803ca2e2d
Paul Hammant <paul@hammant.org>
2025-01-15 22:52:58 +0000

    add debug option

--- sm/boot.sh
+++ sm/boot.sh
@@ -5,6 +5,6 @@
 test_libs=$(find ./test_libs -name '*.jar' | tr '\n' ':')
 cp="${test_libs}./target/classes:./target/test-classes"
 echo "CP\n${cp}\nCP"
-EXEC="java -Djava.security.manager -Djava.security.policy=file:./sm/boot.policy -cp ${cp} tests.SecurityManagerCompositionTests"
+EXEC="java -Djava.security.manager -Djava.security.debug=access:failure -Djava.security.policy=file:./sm/boot.policy -cp ${cp} tests.SecurityManagerCompositionTests"
 echo $EXEC
 $EXEC
